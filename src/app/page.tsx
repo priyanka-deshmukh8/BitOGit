27fd5d725d3aabeae419bc31b6036b294371975b
Priyanka Deshmukh <priyankadeshmukh.h@gmail.com>
2024-10-31 01:22:17 +0530

    Landing Page Modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
-import { span } from "framer-motion/client"
+
 
 export default function LandingPage() {
   return (
